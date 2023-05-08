b0b38cae17b1adac0f068d7c71e160630580ba70
danya7777 <mishindaniil365247@gmail.com>
2023-05-08 09:13:22 +0500

    new

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 
 int main() {
 	char type;
-	printf("Square or circle?(s/c)");	
+	printf("\033[0;36mSquare or circle?(s/c)");	
 	scanf("%c", &type);
 	if(type=='s') {
+	printf("\033[0;31m");
 		    for(int i =1;i<=25;i++){
         printf("*");
         if(i==5||i==10||i==15||i==20) {
@@ -13,17 +14,18 @@ int main() {
     }
     printf("\n");
 	}else if(type=='c') {
+	printf("\033[0;35m");
 		for(int i = 1;i<=24;i++){
 		if(i==3||i==7||i==13||i==19||i==23) {
 			printf("\n");
 		}
 		if(i==1||i==23) {
-			printf("  ");
+			printf("    ");
 		}
 		if(i==3||i==19) {
-			printf(" ");
+			printf("  ");
 		}
-		printf("+");
+		printf("++");
 	}
 	printf("\n");
 	}else{
